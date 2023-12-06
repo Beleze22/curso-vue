2bf5b09cec4606407e337e072b56f28fc388324d
Gustavo <gustavo.beleze@gmail.com>
2023-12-06 02:09:36 -0300

    componentes-intro

--- componentes/componentes-intro/src/main.js
+++ componentes/componentes-intro/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 //eslint-disable-next-line
-import Contador from './Contador-multi.vue'
+import Contadores from './Contadores.vue'
 
 Vue.config.productionTip = false
-Vue.component('app-contador', Contador)
+Vue.component('app-contadores', Contadores)
 
 new Vue({
   render: h => h(App),
